4e501521641b1c406843907d575c3102dd851eb1
KingSaul22 <141522251+KingSaul22@users.noreply.github.com>
2024-11-06 09:21:37 +0100

    JavaDoc - Método Multiplicar
    Typo en el nombre del método

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,14 +2,19 @@ package org.example;
 
 public class Main {
 
-    public static double miltiplicar(int numA, int numB){
+    /**
+     * Una función simple que devuelve el producto de dos números
+     *
+     * @param numA Un número cualquiera
+     * @param numB Otro número cualquiera
+     * @return El producto de los dos números
+     */
+    public static double multiplicar(int numA, int numB){
         return numA*numB;
     }
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
-
 }
 
