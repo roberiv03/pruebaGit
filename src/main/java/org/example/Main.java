810d2fc44ed8be8474b199f6ae66a8e74ffd076b
ihafkul286 <148963396+ihafkul286@users.noreply.github.com>
2024-11-06 08:58:24 +0100

    metodo dividir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
+
+    private static float dividir(float a, float b) {
+        return a / b;
+    }
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
     }
 }
