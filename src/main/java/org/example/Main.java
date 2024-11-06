683bbcdea7d6bfafb450bd5168caf7c66143d71e
Antonio Jose <antonio.j.patrocinio@gmail.com>
2024-11-06 09:04:07 +0100

    Método de multiplicación

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
 public class Main {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static double miltiplicar(int numA, int numB){
+        return numA*numB;
+    }
 }
+
