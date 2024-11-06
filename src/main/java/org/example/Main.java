a3d340041f7432828592f8e7a8f1d61dffe1a217
KingSaul22 <141522251+KingSaul22@users.noreply.github.com>
2024-11-06 09:09:13 +0100

    Método simple - Resta

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,18 @@
 package org.example;
 
 public class Main {
+
+    /**
+     * Función básica que devuelve el resultado de la diferencia entre dos números
+     *
+     * @param num1 Un número cualquiera
+     * @param num2 Otro número cualquiera
+     * @return La diferencia de los dos números
+     */
+    private static double restar(double num1, double num2) {
+        return num1 - num2;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
