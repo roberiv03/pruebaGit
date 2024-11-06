9934c29c87805cd77c0d1cca80a5c9cc73eef9cc
roberiv03 <rrivcon961@g.educaand.es>
2024-11-06 08:59:55 +0100

    método dividir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Main {
+
+    private static int sumar(int a, int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
